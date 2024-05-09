0a2741020928f96e888ecdc76c93fb5091d52d49
davronbekanvarov23 <davronbekanvarov23@gmail.com>
2024-05-09 09:50:00 +0500

    changed

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,10 @@
 import { Link, Form } from "react-router-dom";
 import { FaGoogle } from "react-icons/fa";
 import FormInput from "../components/FormInput";
+import { useSignUp } from "../hooks/useSignUp";
 
 function Login() {
+  const { signUpWithGoogle } = useSignUp();
   return (
     <div className="min h-screen grid place-content-center w-full">
       <div className="mb-3">
@@ -18,7 +20,11 @@ function Login() {
         </Form>
       </div>
       <div>
-        <button type="button" className="btn btn-secondary w-full">
+        <button
+          onClick={signUpWithGoogle}
+          type="button"
+          className="btn btn-secondary w-full"
+        >
           <FaGoogle className="h-5 w-5" />
           Login
         </button>
